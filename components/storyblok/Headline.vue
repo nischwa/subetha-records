2a7d15986acbdebcb13c06d21846b61cd5098a22
Nils Schwarke <nils.schwarke@googlemail.com>
2022-07-02 13:36:12 +0200

    feat(app): enables storyblok bridge

--- components/storyblok/Headline.vue
+++ components/storyblok/Headline.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="headline">{{ headline }}</h1>
+  <h1 class="headline">{{ blok.headline }}</h1>
 </template>
 
 <script lang="ts">
@@ -7,9 +7,17 @@
 import {Component, Prop} from "nuxt-property-decorator";
 import Vue from "vue";
 
+export interface IHeadline {
+  headline: string;
+}
+
 @Component
 export default class Headline extends Vue {
-  @Prop() headline!: string;
+  @Prop() blok!: IHeadline;
+
+  public mounted(): void {
+    console.log(this.blok)
+  }
 }
 </script>
 
