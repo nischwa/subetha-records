b081ea329a54f0eb66cea1510e1d80336a90965a
Schwaddi <nils.schwarke@googlemail.com>
2023-01-16 18:38:24 +0100

    feat(styling): adds initial styling and helpers

---
+++ pages/typography.vue
@@ -0,0 +1,34 @@
+<template>
+  <section style="padding: 1rem;">
+    <h1 class="headline--order-1">Headline Order 1</h1>
+    <p>
+      Above you see a headline with the class <code>headline--order-1</code>
+    </p>
+    <hr>
+    <h2 class="headline--order-2">Headline Order 2</h2>
+    <p>
+      Above you see a headline with the class <code>headline--order-2</code>
+    </p>
+    <hr>
+    <h3 class="headline--order-3">Headline Order 3</h3>
+    <p>
+      Above you see a headline with the class <code>headline--order-3</code>
+    </p>
+    <hr>
+    <h4 class="headline--order-4">Headline Order 4</h4>
+    <p>
+      Above you see a headline with the class <code>headline--order-4</code>
+    </p>
+    <hr>
+    <h5 class="headline--order-5">Headline Order 5</h5>
+    <p>
+      Above you see a headline with the class <code>headline--order-5</code>
+    </p>
+    <hr>
+    <h6 class="headline--order-6">Headline Order 6</h6>
+    <p>
+      Above you see a headline with the class <code>headline--order-6</code>
+    </p>
+    <hr>
+  </section>
+</template>
